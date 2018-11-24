a5d29a1900b4a2b85185713f5fe6be7d72aa8f52
raidou12 <icarodaviduarte@gmail.com>
2018-11-24 04:08:46 -0200

    Commit

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -7,61 +7,64 @@ import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.observer.upload.UploadSizeLimit;
 import br.com.caelum.vraptor.observer.upload.UploadedFile;
 import components.UserSession;
-import repository.FactoryManager;
 import util.FilesUpload;
 
 @Controller
 public class IndexController {
-   
+
 	@Inject
 	private Result result;
 	@Inject
 	private UserSession userSession;
-	
+
 	@Public
-    @Get("/")
-    public void index(){
-		
-    }
+	@Get("/")
+	public void index() {
+
+	}
 
 	@Public
 	@Post("logar")
 	public void login(String email, String password) {
-		if(email.equalsIgnoreCase("jao") && password.equals("123")) {
+		if (email.equalsIgnoreCase("jao") && password.equals("123")) {
 			userSession.setLogged(true);
-			result.redirectTo(HomeController.class).home();;
-		}else {
+			result.redirectTo(HomeController.class).home();
+		} else {
 			result.redirectTo(IndexController.class).index();
 		}
 	}
-	
+
 	@Public
 	@Get("/cadastro")
 	public void register() {
-		
+
 	}
-	
+
 	@Public
 	@Get("sair")
 	public void logOut() {
 		userSession.setLogged(false);
 		result.redirectTo(IndexController.class).index();
 	}
-	
+
 	@Public
 	@Get("/upload")
-	public void upload(UploadedFile fileUpload) {
-		
+	public void upload() {
+
 	}
-	
+
 	@Public
 	@Post("uploadfile")
+	@UploadSizeLimit(sizeLimit=100 * 1024 * 1024, fileSizeLimit=100 * 1024 * 1024)
 	public void uploadfile(UploadedFile fileUpload) {
-			FilesUpload upload = new FilesUpload();
-			upload.upload(fileUpload);
-                        result.redirectTo(HomeController.class).home();
+                System.out.println("---------------Entrou");
+		FilesUpload upload = new FilesUpload();
+		String url = upload.upload(fileUpload);
+                System.out.println(url);
+		result.redirectTo(HomeController.class).home();
 	}
-    
+
 }
--- src/main/java/util/FilesUpload.java
+++ src/main/java/util/FilesUpload.java
@@ -13,29 +13,34 @@ import br.com.caelum.vraptor.observer.upload.UploadedFile;
 
 public class FilesUpload {
 
-	public void upload(UploadedFile fileUpload) {
-			try {
-                            String[] getType = fileUpload.getContentType().split("/");
-                            if(getType[1].equalsIgnoreCase("jpeg")){
-                                getType[1] = "jpg";
-                            }
-                            System.out.println(getType);
-			File file = new File("file", "."+getType[1]);
-                        
+	public String upload(UploadedFile fileUpload) {
+		try {
+			System.out.println(fileUpload.getContentType());
+			String[] getType = fileUpload.getContentType().split("/");
+			if (getType[1].equalsIgnoreCase("jpeg")) {
+				getType[1] = "jpg";
+			}
+			System.out.println(getType);
+			File file = new File("file", "." + getType[1]);
+
 			FileUtils.copyInputStreamToFile(fileUpload.getFile(), file);
                         
-			Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-					"cloud_name", "nerdzonia",
-					"api_key","966118315853311", 
-					"api_secret", "HRv2x14ZTrL-YtfzIsgyrmBIcDw"
-					));
-	
-			Map<String, String> uploadResult = cloudinary.uploader().upload(file, ObjectUtils.asMap("public_id", "Nerdzonia"));
-			
-			System.out.println(uploadResult.get("url")+" "+getType[1]+" "+fileUpload.getContentType());
+			Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap("cloud_name", "nerdzonia", "api_key",
+					"966118315853311", "api_secret", "HRv2x14ZTrL-YtfzIsgyrmBIcDw"));
+                        Map params = null;
+                        UuidGenerator uuid = new UuidGenerator();
+                        if(getType[0].equalsIgnoreCase("video")){
+                            params =  ObjectUtils.asMap("public_id", "Home/video/"+fileUpload.getFileName()+uuid.Uuid(), "resource_type", "video");
+                        }else {
+                            params =  ObjectUtils.asMap("public_id", "Home/photo/"+fileUpload.getFileName()+uuid.Uuid());
+                        }
+    
+			Map<String, String> uploadResult = cloudinary.uploader().upload(file, params);
+                        return uploadResult.get("url");
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+			System.out.println(e.getMessage());
+                        return null;
+		}                
 	}
-
 }
